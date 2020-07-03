a37847c714c63316833f508b7bd9764097d2dfcb
RichBeam <mobile@richbeam.co.kr>
2020-07-03 14:04:54 +0900

    Init

---
+++ app/src/main/java/io/github/ovso/viewbinding/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.ovso.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.ovso.viewbinding.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
